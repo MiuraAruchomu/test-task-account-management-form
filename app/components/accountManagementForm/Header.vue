dada5419a201b8c183b2d496bab0aafb40720fa0
MiuraAruchomu <76885969+MiuraAruchomu@users.noreply.github.com>
2025-10-10 22:25:02 +0400

    added header of form component

---
+++ app/components/accountManagementForm/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center">
+    <h1 class="font-bold text-3xl text-neutral-800">Учетные записи</h1>
+    <button
+      class="w-15 h-15 px-3 py-3 ml-5 text-2xl text-gray-500 hover:text-neutral-800 border border-gray-300 hover:border-neutral-800 rounded-lg hover:cursor-pointer duration-200"
+    >
+      +
+    </button>
+  </div>
+</template>
